023339c520302d4648324f71be748921363b1669
Marc-Velay <marc.velay@gmail.com>
2021-07-03 22:23:48 +0200

    added MLP model training and testing on MNIST dataset

---
+++ vanilly_mlp.py
@@ -0,0 +1,210 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+#from torch.optim import lr_scheduler
+import numpy as np
+from torch.utils import data
+from torchvision import datasets
+from torchvision import models
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+
+import time
+import copy
+import os
+
+import matplotlib.pyplot as plt
+
+print("Executing")
+
+device = 'cpu'
+
+class TwoLayerMLP(nn.Module):
+    
+    def __init__(self, d_in, d_hidden, d_out):
+        super(TwoLayerMLP, self).__init__()
+        self.d_in = d_in
+
+        self.linear1 = nn.Linear(d_in, d_hidden)
+        self.linear2 = nn.Linear(d_hidden, d_hidden)
+        self.linear3 = nn.Linear(d_hidden, d_out)
+
+    def forward(self, X):
+        X = X.view(-1, self.d_in)
+        X = self.linear1(X)
+        X = self.linear2(X)
+        return F.log_softmax(self.linear3(X), dim=1)
+
+def load_mnist(batch_size_train, batch_size_test):
+    
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,)) # Mean and Std of the MNIST dataset
+                       ])),
+        batch_size=batch_size_train, shuffle=True)
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('data', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,)) # Mean and Std of the MNIST dataset
+        ])),
+        batch_size=batch_size_test, shuffle=True)
+    return train_loader, val_loader
+
+def show_data_label_prediction(data, y_true, y_pred=None, shape=(2, 3)):
+    y_pred = [None] * len(y_true) if y_pred is None else y_pred
+    fig = plt.figure()
+    for i in range(np.prod(shape)):
+        plt.subplot(*shape, i+1)
+        plt.tight_layout()
+        plt.imshow(data[i][0], cmap='gray', interpolation='none')
+        plt.title("True: {} Pred: {}".format(y_true[i], y_pred[i]))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+
+def train_val_model(model, criterion, optimizer, dataloaders, num_epochs=25,
+        scheduler=None, log_interval=None):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    # Store losses and accuracies accross epochs
+    losses, accuracies = dict(train=[], val=[]), dict(train=[], val=[])
+
+    for epoch in range(num_epochs):
+        if log_interval is not None and epoch % log_interval == 0:
+            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+            print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            nsamples = 0
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                nsamples += inputs.shape[0]
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            if scheduler is not None and phase == 'train':
+                scheduler.step()
+
+            #nsamples = dataloaders[phase].dataset.data.shape[0]
+            epoch_loss = running_loss / nsamples
+            epoch_acc = running_corrects.double() / nsamples
+
+            losses[phase].append(epoch_loss)
+            accuracies[phase].append(epoch_acc)
+            if log_interval is not None and epoch % log_interval == 0:
+                print('{} Loss: {:.4f} Acc: {:.2f}%'.format(
+                    phase, epoch_loss, 100 * epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+        if log_interval is not None and epoch % log_interval == 0:
+            print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:.2f}%'.format(100 * best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+
+    return model, losses, accuracies
+
+train_loader, val_loader = load_mnist(64, 10000)
+
+dataloaders = dict(train=train_loader, val=val_loader)
+
+# Info about the dataset
+D_in = np.prod(dataloaders["train"].dataset.data.shape[1:])
+D_out = len(dataloaders["train"].dataset.targets.unique())
+print("Datasets shapes:", {x: dataloaders[x].dataset.data.shape for x in ['train', 'val']})
+print("N input features:", D_in, "Output classes:", D_out)
+
+batch_idx, (example_data, example_targets) = next(enumerate(train_loader))
+print("Train batch:", example_data.shape, example_targets.shape)
+batch_idx, (example_data, example_targets) = next(enumerate(val_loader))
+print("Val batch:", example_data.shape, example_targets.shape)
+
+show_data_label_prediction(data=example_data, y_true=example_targets, y_pred=None, shape=(2, 3))
+
+model = TwoLayerMLP(D_in, 50, D_out).to(device)
+print(next(model.parameters()).is_cuda)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+criterion = nn.NLLLoss()
+
+# Explore the model
+for parameter in model.parameters():
+    print(parameter.shape)
+
+print("Total number of parameters =", np.sum([np.prod(parameter.shape) for parameter in model.parameters()]))
+
+model, losses, accuracies = train_val_model(model, criterion, optimizer, dataloaders,
+                       num_epochs=2, log_interval=1)
+
+os.mkdir("outputs")
+torch.save(model.state_dict(), f'outputs/mod-{model.__class__.__name__}.pth')
+
+
+### Test model accuracy
+batch_idx, (example_data, example_targets) = next(enumerate(val_loader))
+example_data = example_data.to(device)
+
+with torch.no_grad():
+  output = model(example_data).cpu()
+
+example_data = example_data.cpu()
+
+# Softmax predictions
+preds = output.argmax(dim=1)
+
+print("Output shape=", output.shape, "label shape=", preds.shape)
+print("Accuracy = {:.2f}%".format((example_targets == preds).sum().item() * 100. / len(example_targets)))
+
+show_data_label_prediction(data=example_data, y_true=example_targets, y_pred=preds, shape=(3, 4))
+
+### Plot wrong predictions
+errors = example_targets != preds
+#print(errors, np.where(errors))
+print(f"Nb errors = {errors.sum()}, (Error rate = {100 * errors.sum().item() / len(errors)}%)")
+err_idx = np.where(errors)[0]
+show_data_label_prediction(data=example_data[err_idx], y_true=example_targets[err_idx],
+                           y_pred=preds[err_idx], shape=(3, 4))
+
+print("Done")
